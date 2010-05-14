679e78ffce056c3b1a64619b8b1a41db1d6cf7e1
admin <admin@561ebbd9-64b5-42ff-b8f9-4563525389ac>
2010-05-14 21:15:41 +0000

    Update to latest DEV version of lightbox2

--- sites/all/modules/lightbox2/js/lightbox.js
+++ sites/all/modules/lightbox2/js/lightbox.js
@@ -1,4 +1,4 @@
-/* $Id: lightbox.js,v 1.5.2.6.2.114 2009/01/05 14:21:52 snpower Exp $ */
+/* $Id: lightbox.js,v 1.5.2.6.2.121 2010/03/07 17:32:30 snpower Exp $ */
 
 /**
  * jQuery Lightbox
@@ -18,6 +18,7 @@
  */
 
 var Lightbox = {
+  auto_modal : false,
   overlayOpacity : 0.8, // Controls transparency of shadow overlay.
   overlayColor : '000', // Controls colour of shadow overlay.
   disableCloseClick : true,
@@ -199,11 +200,15 @@ var Lightbox = {
   // Loops through anchor tags looking for 'lightbox', 'lightshow' and
   // 'lightframe', etc, references and applies onclick events to appropriate
   // links. You can rerun after dynamically adding images w/ajax.
-  initList : function() {
+  initList : function(context) {
+
+    if (context == undefined || context == null) {
+      context = document;
+    }
 
     // Attach lightbox to any links with rel 'lightbox', 'lightshow' or
     // 'lightframe', etc.
-    $("a[@rel^='lightbox']:not(.lightbox-processed), area[@rel^='lightbox']:not(.lightbox-processed)").addClass('lightbox-processed').click(function(e) {
+    $("a[rel^='lightbox']:not(.lightbox-processed), area[rel^='lightbox']:not(.lightbox-processed)", context).addClass('lightbox-processed').click(function(e) {
       if (Lightbox.disableCloseClick) {
         $('#lightbox').unbind('click');
         $('#lightbox').click(function() { Lightbox.end('forceClose'); } );
@@ -212,7 +217,7 @@ var Lightbox = {
       if (e.preventDefault) { e.preventDefault(); }
       return false;
     });
-    $("a[@rel^='lightshow']:not(.lightbox-processed), area[@rel^='lightshow']:not(.lightbox-processed)").addClass('lightbox-processed').click(function(e) {
+    $("a[rel^='lightshow']:not(.lightbox-processed), area[rel^='lightshow']:not(.lightbox-processed)", context).addClass('lightbox-processed').click(function(e) {
       if (Lightbox.disableCloseClick) {
         $('#lightbox').unbind('click');
         $('#lightbox').click(function() { Lightbox.end('forceClose'); } );
@@ -221,7 +226,7 @@ var Lightbox = {
       if (e.preventDefault) { e.preventDefault(); }
       return false;
     });
-    $("a[@rel^='lightframe']:not(.lightbox-processed), area[@rel^='lightframe']:not(.lightbox-processed)").addClass('lightbox-processed').click(function(e) {
+    $("a[rel^='lightframe']:not(.lightbox-processed), area[rel^='lightframe']:not(.lightbox-processed)", context).addClass('lightbox-processed').click(function(e) {
       if (Lightbox.disableCloseClick) {
         $('#lightbox').unbind('click');
         $('#lightbox').click(function() { Lightbox.end('forceClose'); } );
@@ -231,7 +236,7 @@ var Lightbox = {
       return false;
     });
     if (Lightbox.enableVideo) {
-      $("a[@rel^='lightvideo']:not(.lightbox-processed), area[@rel^='lightvideo']:not(.lightbox-processed)").addClass('lightbox-processed').click(function(e) {
+      $("a[rel^='lightvideo']:not(.lightbox-processed), area[rel^='lightvideo']:not(.lightbox-processed)", context).addClass('lightbox-processed').click(function(e) {
         if (Lightbox.disableCloseClick) {
           $('#lightbox').unbind('click');
           $('#lightbox').click(function() { Lightbox.end('forceClose'); } );
@@ -241,7 +246,14 @@ var Lightbox = {
         return false;
       });
     }
-    $("a[@rel^='lightmodal']:not(.lightbox-processed), area[@rel^='lightmodal']:not(.lightbox-processed)").addClass('lightbox-processed').click(function(e) {
+    $("a[rel^='lightmodal']:not(.lightbox-processed), area[rel^='lightmodal']:not(.lightbox-processed)", context).addClass('lightbox-processed').click(function(e) {
+      $('#lightbox').unbind('click');
+      Lightbox.start(this, false, false, false, true);
+      if (e.preventDefault) { e.preventDefault(); }
+      return false;
+    });
+    $("#lightboxAutoModal:not(.lightbox-processed)", context).addClass('lightbox-processed').click(function(e) {
+      Lightbox.auto_modal = true;
       $('#lightbox').unbind('click');
       Lightbox.start(this, false, false, false, true);
       if (e.preventDefault) { e.preventDefault(); }
@@ -332,15 +344,25 @@ var Lightbox = {
         // Loop through anchors and add them to imageArray.
         for (i = 0; i < anchors.length; i++) {
           anchor = anchors[i];
-          if (anchor.href && $(anchor).attr('rel')) {
+          if (anchor.href && typeof(anchor.href) == "string" && $(anchor).attr('rel')) {
             var rel_data = Lightbox.parseRel(anchor);
             var anchor_title = (rel_data["title"] ? rel_data["title"] : anchor.title);
+            img_alt = anchor.title;
+            if (!img_alt) {
+              var anchor_img = $(anchor).find("img");
+              if (anchor_img && $(anchor_img).attr("alt")) {
+                img_alt = $(anchor_img).attr("alt");
+              }
+              else {
+                img_alt = title;
+              }
+            }
             if (rel_data["rel"] == rel) {
               if (rel_data["group"] == rel_group) {
                 if (Lightbox.isLightframe || Lightbox.isModal) {
                   rel_style = rel_data["style"];
                 }
-                Lightbox.imageArray.push([anchor.href, anchor_title, alt, rel_style]);
+                Lightbox.imageArray.push([anchor.href, anchor_title, img_alt, rel_style]);
               }
             }
           }
@@ -601,8 +623,7 @@ var Lightbox = {
       }
       else {
         if (Lightbox.isVideo) {
-          $("#modalContainer").html(Lightbox.modalHTML);
-          $("#modalContainer").click(function() { return false; } );
+          $("#modalContainer").html(Lightbox.modalHTML).click(function(){return false;}).css('zIndex', '10500').show();
         }
         else {
           var src = unescape(Lightbox.imageArray[Lightbox.activeImage][0]);
@@ -610,18 +631,20 @@ var Lightbox = {
             $(src).appendTo("#modalContainer");
           }
           else {
-            $("#modalContainer").load(src);
+            // Use a callback to show the new image, otherwise you get flicker.
+            $("#modalContainer").hide().load(src, function () {$('#modalContainer').css({'zIndex': '10500'}).show();});
           }
           $('#modalContainer').unbind('click');
         }
-        $('#modalContainer').css({'zIndex': '10500'}).show();
+        // This might be needed in the Lightframe section above.
+        //$('#modalContainer').css({'zIndex': '10500'}).show();
       }
     }
 
     // Handle display of image content.
     else {
       $('#imageContainer').show();
-      if($.browser.safari) {
+      if ($.browser.safari) {
         $('#lightboxImage').css({'zIndex': '10500'}).show();
       }
       else {
@@ -760,7 +783,9 @@ var Lightbox = {
 
       // If not first image in set, display prev image button.
       if ((Lightbox.total > 1 && Lightbox.loopItems) || Lightbox.activeImage !== 0) {
-        $(prevLink).css({'zIndex': '10500'}).show().click(function() {
+        // Unbind any other click handlers, otherwise this adds a new click handler
+        // each time the arrow is clicked.
+        $(prevLink).css({'zIndex': '10500'}).show().unbind().click(function() {
           Lightbox.changeData(Lightbox.activeImage - 1); return false;
         });
       }
@@ -771,7 +796,9 @@ var Lightbox = {
 
       // If not last image in set, display next image button.
       if ((Lightbox.total > 1 && Lightbox.loopItems) || Lightbox.activeImage != (Lightbox.total - 1)) {
-        $(nextLink).css({'zIndex': '10500'}).show().click(function() {
+        // Unbind any other click handlers, otherwise this adds a new click handler
+        // each time the arrow is clicked.
+        $(nextLink).css({'zIndex': '10500'}).show().unbind().click(function() {
           Lightbox.changeData(Lightbox.activeImage + 1); return false;
         });
       }
@@ -891,7 +918,10 @@ var Lightbox = {
       $('#frameContainer').empty().hide();
     }
     else if (Lightbox.isVideo || Lightbox.isModal) {
-      $('#modalContainer').hide().html("");
+      if (!Lightbox.auto_modal) {
+        $('#modalContainer').hide().html("");
+      }
+      Lightbox.auto_modal = false;
     }
   },
 
@@ -924,20 +954,20 @@ var Lightbox = {
   // Returns array with page width, height and window width, height.
   // Core code from - quirksmode.com.
   // Edit for Firefox by pHaez.
+
   getPageSize : function() {
 
     var xScroll, yScroll;
 
-    if (document.body.scrollHeight > document.body.offsetHeight) { // all but Explorer Mac
-      xScroll = document.body.scrollWidth;
-      yScroll = document.body.scrollHeight;
-    }
-    else if (window.innerHeight && window.scrollMaxY) {
+    if (window.innerHeight && window.scrollMaxY) {
       xScroll = window.innerWidth + window.scrollMaxX;
       yScroll = window.innerHeight + window.scrollMaxY;
     }
-    // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari.
-    else {
+    else if (document.body.scrollHeight > document.body.offsetHeight) { // All but Explorer Mac.
+      xScroll = document.body.scrollWidth;
+      yScroll = document.body.scrollHeight;
+    }
+    else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari.
       xScroll = document.body.offsetWidth;
       yScroll = document.body.offsetHeight;
     }
@@ -953,8 +983,7 @@ var Lightbox = {
       }
       windowHeight = self.innerHeight;
     }
-    // Explorer 6 Strict Mode.
-    else if (document.documentElement && document.documentElement.clientHeight) {
+    else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode.
       windowWidth = document.documentElement.clientWidth;
       windowHeight = document.documentElement.clientHeight;
     }
@@ -962,26 +991,21 @@ var Lightbox = {
       windowWidth = document.body.clientWidth;
       windowHeight = document.body.clientHeight;
     }
-
-
-    // For small pages with total height less then height of the viewport.
+    // For small pages with total height less than height of the viewport.
     if (yScroll < windowHeight) {
       pageHeight = windowHeight;
     }
     else {
       pageHeight = yScroll;
     }
-
-
-    // For small pages with total width less then width of the viewport.
+    // For small pages with total width less than width of the viewport.
     if (xScroll < windowWidth) {
-      pageWidth = windowWidth;
-    }
-    else {
       pageWidth = xScroll;
     }
-
-    arrayPageSize = [pageWidth, pageHeight, windowWidth, windowHeight];
+    else {
+      pageWidth = windowWidth;
+    }
+    arrayPageSize = new Array(pageWidth,pageHeight,windowWidth,windowHeight);
     return arrayPageSize;
   },
 
@@ -1086,10 +1110,10 @@ var Lightbox = {
   triggerLightbox: function (rel_type, rel_group) {
     if (rel_type.length) {
       if (rel_group && rel_group.length) {
-        $("a[@rel^='" + rel_type +"\[" + rel_group + "\]'], area[@rel^='" + rel_type +"\[" + rel_group + "\]']").eq(0).trigger("click");
+        $("a[rel^='" + rel_type +"\[" + rel_group + "\]'], area[rel^='" + rel_type +"\[" + rel_group + "\]']").eq(0).trigger("click");
       }
       else {
-        $("a[@rel^='" + rel_type +"'], area[@rel^='" + rel_type +"']").eq(0).trigger("click");
+        $("a[rel^='" + rel_type +"'], area[rel^='" + rel_type +"']").eq(0).trigger("click");
       }
     }
   },
@@ -1116,11 +1140,11 @@ var Lightbox = {
 Drupal.behaviors.initLightbox = function (context) {
   $('body:not(.lightbox-processed)', context).addClass('lightbox-processed').each(function() {
     Lightbox.initialize();
-    $('#lightboxAutoModal').triggerHandler('click');
     return false; // Break the each loop.
   });
 
   // Attach lightbox to any links with lightbox rels.
-  Lightbox.initList();
+  Lightbox.initList(context);
+  $('#lightboxAutoModal', context).triggerHandler('click');
 };
 
